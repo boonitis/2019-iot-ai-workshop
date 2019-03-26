db4f72cb5ee3c3e3abefe6c8fe5869cdfc7822e0
Nitis Monburinon <boonitis@gmail.com>
2019-03-26 09:46:41 +0900

    Update

--- recognition.py
+++ recognition.py
@@ -1,207 +1,103 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+from __future__ import absolute_import, division, print_function
 
-"""Simple image classification with Inception.
-
-Run image classification with Inception trained on ImageNet 2012 Challenge data
-set.
-
-This program creates a graph from a saved GraphDef protocol buffer,
-and runs inference on an input JPEG image. It outputs human readable
-strings of the top 5 predictions along with their probabilities.
-
-Change the --image_file argument to any jpg image to compute a
-classification of that image.
-
-Please see the tutorial and website for a detailed description of how
-to use this script to perform image recognition.
-
-https://tensorflow.org/tutorials/image_recognition/
-"""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import argparse
-import os.path
-import re
-import sys
-import tarfile
-
-import numpy as np
-from six.moves import urllib
 import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import PIL.Image as Image
+from tensorflow.keras import layers
 
-FLAGS = None
+##################################################################################
+"""call this module to setup your python packages via pip"""
 
-# pylint: disable=line-too-long
-DATA_URL = 'file:/home/boon/Desktop/2019-iot-ai-workshop/saved_models/1553548470/saved_model.pb'
-# pylint: enable=line-too-long
+from pip._internal import main as pip
 
+pip_install_argument = "install"
 
-class NodeLookup(object):
-  """Converts integer node ID's to human readable labels."""
+# packages to install
+packages_to_install = [
+        "numpy",        # math magic 1
+        "scipy",        # math magic 2
+        "tensorflow_hub",
+        ]
 
-  def __init__(self,
-               label_lookup_path=None,
-               uid_lookup_path=None):
-    if not label_lookup_path:
-      label_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
-    if not uid_lookup_path:
-      uid_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_synset_to_human_label_map.txt')
-    self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
-
-  def load(self, label_lookup_path, uid_lookup_path):
-    """Loads a human readable English name for each softmax node.
+def install(packages):
+    """installes given packages via pip
 
     Args:
-      label_lookup_path: string UID to integer node ID.
-      uid_lookup_path: string UID to human-readable string.
+        package names as list
 
     Returns:
-      dict from integer node ID to human-readable string.
+        None
+
     """
-    if not tf.gfile.Exists(uid_lookup_path):
-      tf.logging.fatal('File does not exist %s', uid_lookup_path)
-    if not tf.gfile.Exists(label_lookup_path):
-      tf.logging.fatal('File does not exist %s', label_lookup_path)
+    global pip_install_argument
+    for package in packages:
+        pip([pip_install_argument, package])
 
-    # Loads mapping from string UID to human-readable string
-    proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
-    uid_to_human = {}
-    p = re.compile(r'[n\d]*[ \S,]*')
-    for line in proto_as_ascii_lines:
-      parsed_items = p.findall(line)
-      uid = parsed_items[0]
-      human_string = parsed_items[2]
-      uid_to_human[uid] = human_string
+install(packages_to_install)
 
-    # Loads mapping from string UID to integer node ID.
-    node_id_to_uid = {}
-    proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
-    for line in proto_as_ascii:
-      if line.startswith('  target_class:'):
-        target_class = int(line.split(': ')[1])
-      if line.startswith('  target_class_string:'):
-        target_class_string = line.split(': ')[1]
-        node_id_to_uid[target_class] = target_class_string[1:-2]
-
-    # Loads the final mapping of integer node ID to human-readable string
-    node_id_to_name = {}
-    for key, val in node_id_to_uid.items():
-      if val not in uid_to_human:
-        tf.logging.fatal('Failed to locate: %s', val)
-      name = uid_to_human[val]
-      node_id_to_name[key] = name
-
-    return node_id_to_name
-
-  def id_to_string(self, node_id):
-    if node_id not in self.node_lookup:
-      return ''
-    return self.node_lookup[node_id]
+###########################################################
 
 
-def create_graph():
-  """Creates a graph from saved GraphDef file and returns a saver."""
-  # Creates graph from saved graph_def.pb.
-  with tf.gfile.FastGFile(os.path.join(
-      'saved_models/1553546856', 'saved_model.pb'), 'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
+data_root = tf.keras.utils.get_file(
+    'flower_photos','/home/boon/Desktop/2019-iot-ai-workshop/flower_photos/',
+    untar=True)
+
+image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255)
+image_data = image_generator.flow_from_directory(str(data_root))
+
+for image_batch,label_batch in image_data:
+    print("Image batch shape: ", image_batch.shape)
+    print("Labe batch shape: ", label_batch.shape)
+    break
+
+classifier_url = "https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/classification/2" #@param {type:"string"}
+
+def classifier(x):
+    classifier_module = hub.Module(classifier_url)
+    return classifier_module(x)
+  
+IMAGE_SIZE = hub.get_expected_image_size(hub.Module(classifier_url))
+
+classifier_layer = layers.Lambda(classifier, input_shape = IMAGE_SIZE+[3])
+classifier_model = tf.keras.Sequential([classifier_layer])
+classifier_model.summary()
+
+image_data = image_generator.flow_from_directory(str(data_root), target_size=IMAGE_SIZE)
+for image_batch,label_batch in image_data:
+    print("Image batch shape: ", image_batch.shape)
+    print("Labe batch shape: ", label_batch.shape)
+    break
+
+import tensorflow.keras.backend as K
+sess = K.get_session()
+init = tf.global_variables_initializer()
+
+sess.run(init)
 
 
-def run_inference_on_image(image):
-  """Runs inference on an image.
 
-  Args:
-    image: Image file name.
+########################################################
 
-  Returns:
-    Nothing
-  """
-  if not tf.gfile.Exists(image):
-    tf.logging.fatal('File does not exist %s', image)
-  image_data = tf.gfile.FastGFile(image, 'rb').read()
+grace_hopper = tf.keras.utils.get_file('monkey.jpg','file://home/boon/Desktop/2019-iot-ai-workshop/test/')
+grace_hopper = Image.open(grace_hopper).resize(IMAGE_SIZE)
+grace_hopper 
 
-  # Creates graph from saved GraphDef.
-  create_graph()
+grace_hopper = np.array(grace_hopper)/255.0
+grace_hopper.shape
 
-  with tf.Session() as sess:
-    # Some useful tensors:
-    # 'softmax:0': A tensor containing the normalized prediction across
-    #   1000 labels.
-    # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
-    #   float description of the image.
-    # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
-    #   encoding of the image.
-    # Runs the softmax tensor by feeding the image_data as input to the graph.
-    softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-    predictions = sess.run(softmax_tensor,
-                           {'DecodeJpeg/contents:0': image_data})
-    predictions = np.squeeze(predictions)
+result = classifier_model.predict(grace_hopper[np.newaxis, ...])
+result.shape
 
-    # Creates node ID --> English string lookup.
-    node_lookup = NodeLookup()
+predicted_class = np.argmax(result[0], axis=-1)
+predicted_acc = str(100)
+predicted_class
 
-    top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
-    for node_id in top_k:
-      human_string = node_lookup.id_to_string(node_id)
-      score = predictions[node_id]
-      print('%s (score = %.5f)' % (human_string, score))
+labels_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+imagenet_labels = np.array(open(labels_path).read().splitlines())
 
 
-def main(_):
-  image = (FLAGS.image_file if FLAGS.image_file else
-           os.path.join('monkey.jpg'))
-  run_inference_on_image(image)
+result = imagenet_labels[predicted_class] + " : " + predicted_acc
 
-
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  # classify_image_graph_def.pb:
-  #   Binary representation of the GraphDef protocol buffer.
-  # imagenet_synset_to_human_label_map.txt:
-  #   Map from synset ID to a human readable string.
-  # imagenet_2012_challenge_label_map_proto.pbtxt:
-  #   Text representation of a protocol buffer mapping a label to synset ID.
-  parser.add_argument(
-      '--model_dir',
-      type=str,
-      default='/tmp/imagenet',
-      help="""\
-      Path to classify_image_graph_def.pb,
-      imagenet_synset_to_human_label_map.txt, and
-      imagenet_2012_challenge_label_map_proto.pbtxt.\
-      """
-  )
-  parser.add_argument(
-      '--image_file',
-      type=str,
-      default='',
-      help='Absolute path to image file.'
-  )
-  parser.add_argument(
-      '--num_top_predictions',
-      type=int,
-      default=5,
-      help='Display this many predictions.'
-  )
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+print("\nClassification Result")
+print(result)
