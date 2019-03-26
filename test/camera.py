2ba4706ac4238ddd2163fd618d9ea1b9b544338f
Nitis Monburinon <boonitis@gmail.com>
2019-03-26 14:04:23 +0900

    a

---
+++ test/camera.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+import time
+import glob
+from datetime import datetime
+from itertools import count
+
+import RPi.GPIO as GPIO
+from modules import photograph
+from modules.clock import Clock
+from picamera import PiCamera
+
+# Setup GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, GPIO.PUD_DOWN)
+
+# Setup PI camera
+camera = PiCamera()
+camera.resolution = (1600, 1200)
+camera.start_preview()
+sleep(10)
+camera.stop_preview()
