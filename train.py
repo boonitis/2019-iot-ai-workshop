38d33203792f7c1fb3954aba9d0e68f45b13d0a4
Nitis Monburinon <boonitis@gmail.com>
2019-03-26 10:39:28 +0900

    update

--- train.py
+++ train.py
@@ -1,11 +1,17 @@
 import os
 import subprocess
+
+# Train model on the server
+
 command = "ssh -t boon@192.168.11.12 'python3 /home/boon/Desktop/2019-iot-ai-workshop/process.py'"
 os.system(command)
 result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 #returns result but must be read individualy via
 #result.stdout.read()#returns output
 #result.stderr.read()#returns error
-command = "scp boon@192.168.11.12:/home/boon/Desktop/2019-iot-ai-workshop/saved_models/* /home/pi/2019-iot-ai-workshop/saved_models/"
+
+# Copy trained models to local
+
+command = "scp -rp boon@192.168.11.12:/home/boon/Desktop/2019-iot-ai-workshop/saved_models/* /home/pi/2019-iot-ai-workshop/saved_models/"
 os.system(command)
 result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
