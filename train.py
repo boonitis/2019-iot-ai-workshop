b4f86e352e6b9c114bb7b6e39c2c02944e9efc52
Nitis Monburinon <boonitis@gmail.com>
2019-03-26 15:36:56 +0900

    a

--- train.py
+++ train.py
@@ -1,18 +1,6 @@
 import os
 import subprocess
 
-
-# Upload 
-
-command = "ssh -t boon@192.168.11.12 'rm -rf /home/boon/Desktop/2019-iot-ai-workshop/training/*'"
-os.system(command)
-result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-
-
-command = "scp -r /home/pi/2019-iot-ai-workshop/training/ boon@192.168.11.12:/home/boon/Desktop/2019-iot-ai-workshop/training/"
-os.system(command)
-result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-
 # Train model on the server
 
 command = "ssh -t boon@192.168.11.12 'python3 /home/boon/Desktop/2019-iot-ai-workshop/process.py'"
@@ -21,9 +9,3 @@ result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=su
 #returns result but must be read individualy via
 #result.stdout.read()#returns output
 #result.stderr.read()#returns error
-
-# Copy trained models to local
-
-command = "scp -r boon@192.168.11.12:/home/boon/Desktop/2019-iot-ai-workshop/saved_models/ /home/pi/2019-iot-ai-workshop/saved_models/"
-os.system(command)
-result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
