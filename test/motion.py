d43c28644dc7bf67cefc2f11dbb8a51c093fe413
Nitis Monburinon <boonitis@gmail.com>
2019-03-26 14:13:33 +0900

    m

--- test/motion.py
+++ test/motion.py
@@ -1,30 +1,64 @@
-"""call this module to setup your python packages via pip"""
+#!/usr/bin/env python3
 
-from pip._internal import main as pip
+import json
+import os
+import sys
+import time
+import glob
+from datetime import datetime
+from itertools import count
 
-pip_install_argument = "install"
+import RPi.GPIO as GPIO
+from modules import photograph
+from modules.clock import Clock
+from picamera import PiCamera
 
-# packages to install
-packages_to_install = [
-        "tensorflow"
-        "numpy",        # math magic 1
-        "scipy",        # math magic 2
-        "tensorflow_hub",
-        ]
+# Setup GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, GPIO.PUD_DOWN)
 
-def install(packages):
-    """installes given packages via pip
+# Setup PI camera
+camera = PiCamera()
+camera.resolution = (1600, 1200)
 
-    Args:
-        package names as list
+# Setup Performance Timers
+timer_all = Clock()
 
-    Returns:
-        None
+# Initialize the counter
+counter = count(start=0,step=1)
 
-    """
-    global pip_install_argument
-    for package in packages:
-        pip([pip_install_argument, package])
+option = ""
+if len(sys.argv) > 1:
+    for arg in sys.argv:
+        if arg[0] == "-":
+            option = arg[1:]
 
-if __name__ == '__main__':
-    install(packages_to_install)
+
+
+# Main code goes below here
+try:
+    while True:
+
+        detect = GPIO.input(4)
+
+        # When PIR sensor detect motion
+        if(detect == 1):
+
+            print(next(counter))
+            # Start a timer for the whole process
+            print("Motion detected")
+
+            # file_path = 'test-monkey.jpg'
+
+        print(next(counter))
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("\nKeyboardInterrupt: Exit by user")
+    camera.close()
+    GPIO.cleanup()
+except Exception as e:
+    print("Errors occured while Kakashi is running: %s" % e)
+    camera.close()
+    GPIO.cleanup()
