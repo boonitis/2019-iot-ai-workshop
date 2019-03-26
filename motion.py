ec493aa6061587cee4762775f7710fa27f578617
Nitis Monburinon <boonitis@gmail.com>
2019-03-26 14:08:47 +0900

    m

--- motion.py
+++ motion.py
@@ -1,33 +1,4 @@
-"""call this module to setup your python packages via pip"""
 
-from pip._internal import main as pip
-
-pip_install_argument = "install"
-
-# packages to install
-packages_to_install = [
-        "tensorflow"
-        "numpy",        # math magic 1
-        "scipy",        # math magic 2
-        "tensorflow_hub",
-        ]
-
-def install(packages):
-    """installes given packages via pip
-
-    Args:
-        package names as list
-
-    Returns:
-        None
-
-    """
-    global pip_install_argument
-    for package in packages:
-        pip([pip_install_argument, package])
-
-if __name__ == '__main__':
-    install(packages_to_install)
 
 #!/usr/bin/env python3
 
